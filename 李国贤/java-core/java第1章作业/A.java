012f027876eda3e87f361482d8f354b821075a73
liguoxian01 <2991935482@QQ.com>
2019-05-09 20:21:20 +0800

    上交java第一章作业

---
+++ 李国贤/java-core/java第1章作业/A.java
@@ -0,0 +1,47 @@
+public class A{//封面
+	public static void main (String[] args){//主要章节开始
+
+	        //主要内容---你要计算机干的事 
+	
+        	//向控制台输出一句话：小样 这是我写的第一个java程序。
+
+ 
+	        System.out.println("小样 这是我写的第一个java程序");
+       		
+		System.out.println("我的姓名\t我的性别\t我的年龄");
+		System.out.println("小样\t\t男\t\t18");
+		
+   		System.out.println("\t\t\t*");
+		System.out.println("\t\t*\t*\t*\n\t*\t*\t*\t*\t*\n*\t*\t*\t*\t*\t*\t*");
+		
+		System.out.println("日\t一\t二\t三\t四\t五\t六");
+		System.out.println("\t\t\t1\t2\t3\t4\n5\t6\t7\t8\t9\t10\t11\n12\t13\t14\t15\t16\t17\t18");
+		
+		System.out.println("**************************************");
+		System.out.println("\t人事管理系统\n\t1、添加用户\n\t2、查询用户\n\t3、退出系统");		
+		System.out.println("**************************************");
+		
+		System.out.println("┏━━━━━━━━━━━━━┒");
+		System.out.println("┃                          ┃\n");
+		System.out.println("┃                          ┃\n");
+		System.out.println("┃\t\"图书管理系统\"\t    ┃\n");
+		System.out.println("┃                          ┃\n");
+		System.out.println("┃\t制作人：\"小样\"\t    ┃\n");
+		System.out.println("┃                          ┃\n");
+		System.out.println("┃                          ┃");
+		System.out.println("┗━━━━━━━━━━━━━┛");
+
+		
+
+		System.out.println("\t\t购物清单");
+		System.out.println("商品名称\t购买数量\t商品单价\t金额");
+		System.out.println("鳄鱼夹克\t1\t\t1900\t\t1900\ninodMP4\t\t2\t\t400\t\t800");
+
+		System.out.println("\t欢迎使用我行我素购物管理系统\n\t\t1.登录系统\n\t\t2.退出");
+		System.out.println("*********************************************");		
+		System.out.println("*********************************************");
+		System.out.println("\t\t1.客户信息管理\n\t\t2.购物结算\n\t\t3.真情回馈\n\t\t4.注销");
+		System.out.println("*********************************************");
+
+		}//主要章节结束
+}//封底
